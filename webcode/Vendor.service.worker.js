c27aad27679170d780e840596e44dfd0b3a094a9
DIGI Byte <6645396+digimbyte@users.noreply.github.com>
2024-12-25 08:23:18 +1000

    fixed export and admin trash

--- webcode/Vendor.service.worker.js
+++ webcode/Vendor.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1735072840|1402263652';
+const CACHE_VERSION = '1735078817|28038664';
 /** @type {string} */
 const CACHE_PREFIX = 'Vendor-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
